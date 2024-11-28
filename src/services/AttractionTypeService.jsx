a75802ea883226e6010098bc55628d729a40c776
H03 <hieupnse171321@fpt.edu.vn>
2024-11-28 21:18:21 +0700

    update manage all category

--- src/services/AttractionTypeService.jsx
+++ src/services/AttractionTypeService.jsx
@@ -2,10 +2,13 @@ import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL;
 import { getCookie } from '@services/AuthenService';
 
-export const fetchAttractionType = async () => {
+export const fetchAttractionType = async (nameSearch = '') => {
     const token = getCookie('token');
     try {
         const response = await axios.get(`${baseURL}/api/attraction-types`, {
+            params: {
+                nameSearch: nameSearch
+            },
             headers: {
                 'Authorization': `Bearer ${token}`
             }
@@ -17,7 +20,40 @@ export const fetchAttractionType = async () => {
             createdAt: item.createdAt
         }));
     } catch (error) {
-        console.error('Error fetching tour templates:', error);
+        console.error('Error fetching attraction types:', error);
+        throw error;
+    }
+};
+
+export const createAttractionType = async (attractionData) => {
+    const token = getCookie('token');
+    try {
+        const response = await axios.post(`${baseURL}/api/attraction-types`, {
+            attractionTypeName: attractionData.name,
+            description: attractionData.description
+        }, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error creating attraction type:', error);
+        throw error;
+    }
+};
+
+export const deleteAttractionType = async (attractionTypeId) => {
+    const token = getCookie('token');
+    try {
+        const response = await axios.delete(`${baseURL}/api/attraction-types/${attractionTypeId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting attraction type:', error);
         throw error;
     }
 };
--- src/services/PostCategoryService.jsx
+++ src/services/PostCategoryService.jsx
@@ -1,9 +1,18 @@
 import axios from 'axios';
 const baseURL = import.meta.env.VITE_API_URL;
+import { getCookie } from '@services/AuthenService';
 
-export const fetchPostCategory = async () => {
+export const fetchPostCategory = async (nameSearch = '') => {
+    const token = getCookie('token');
     try {
-        const response = await axios.get(`${baseURL}/api/post-categories`);
+        const response = await axios.get(`${baseURL}/api/post-categories`, {
+            params: {
+                nameSearch: nameSearch
+            },
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
         return response.data.data.map(item => ({
             postCategoryId: item.postCategoryId,
             name: item.name,
@@ -11,7 +20,41 @@ export const fetchPostCategory = async () => {
             createdAt: item.createdAt
         }));
     } catch (error) {
-        console.error('Error fetching tour templates:', error);
+        console.error('Error fetching tour categories:', error);
+        throw error;
+    }
+};
+
+
+export const createPostCategory = async (categoryData) => {
+    const token = getCookie('token');
+    try {
+        const response = await axios.post(`${baseURL}/api/post-categories`, {
+            name: categoryData.name,
+            description: categoryData.description
+        }, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error creating post category:', error);
+        throw error;
+    }
+};
+
+export const deletePostCategory = async (postCategoryId) => {
+    const token = getCookie('token');
+    try {
+        const response = await axios.delete(`${baseURL}/api/post-categories/${postCategoryId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        return response.data;
+    } catch (error) {
+        console.error('Error deleting post category:', error);
         throw error;
     }
 };
